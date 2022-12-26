246c66bd1c938532cd502d19c3147193775daaaa
Peter Tuan Anh <petertuananh@hotmail.com>
2022-12-26 17:41:55 +0700

    Removed some unnecessary libs

--- shard.js
+++ shard.js
@@ -1,14 +1,8 @@
 const config = require("./config.json")
 const Discord = require('discord.js');
 const ascii = require('ascii-table');
-const express = require("express");
-const app = express()
-const systemInfo = require("systeminformation");
-const moment = require("moment");
-require("moment-duration-format");
 let table = new ascii("Shard status");
 table.setHeading("Id", "Status");
-const exec = require('node:child_process').exec;
 const manager = new Discord.ShardingManager('./yourauth.js', {
     token: config.bot.basic.token,
     totalShards: config.setting.shard,
